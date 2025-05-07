72ccf2c3e95fbc717289058c775bea1411ad49fd
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-05-07 10:40:20 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -56,8 +56,22 @@ const App = () => {
 
 export default App;
 */
-const App = () => {
-  return <div></div>;
+import { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
